2c6b34f843b682a14f374696c17bec159e3c7fa2
Sarah Wever <sarahewever@gmail.com>
2018-06-27 21:35:11 -0400

    fix port

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(path.join(__dirname, "static")));
 
-var server = app.listen(8000, function() {
+var server = app.listen(process.env.PORT || 8000, function() {
   console.log("Express is running on port 8000");
 });
 
 var io = require("socket.io")(server);
 
-// var neurosky = require("node-neurosky");
+var neurosky = require("node-neurosky");
 
-// var client = neurosky.createClient({
-//   appName: "NeuroSky",
-//   appKey: "0fc4141b4b45c675cc8d3a765b8d71c5bde9390"
-// });
+var client = neurosky.createClient({
+  appName: "NeuroSky",
+  appKey: "0fc4141b4b45c675cc8d3a765b8d71c5bde9390"
+});
 
 var $socket;
 io.on("connection", function(socket) {
@@ -34,9 +34,9 @@ io.on("connection", function(socket) {
 var meditationvalue;
 var meditationarray = [];
 
-// client.on("data", function(data) {
-//   console.log(data);
+client.on("data", function(data) {
+  console.log(data);
 
-//   $socket.emit("neurosky", data);
-//   meditationvalue = data.eSense.meditation;
-// });
+  $socket.emit("neurosky", data);
+  meditationvalue = data.eSense.meditation;
+});
