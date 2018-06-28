c61055338f384ad1cdd3bb2b4264bdacd5861c92
Sarah Wever <sarahewever@gmail.com>
2018-06-27 22:49:59 -0400

    start demo mode

---
+++ static/scripts/demo.js
@@ -0,0 +1,9 @@
+// function setup() {
+//   console.log("setup");
+//   var mycanvas = createCanvas(500, 300);
+//   mycanvas.parent("demo-controls");
+// }
+
+// function draw() {
+//   console.log("draw loop");
+// }
